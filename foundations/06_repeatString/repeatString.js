aa28ad911b6cd2c72aad8e132aea062b85c73a04
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-03 16:17:27 +0100

    Completed 06-RepeatString
    Created a function that mimick the prototype.String.repeat() method to show loop mastery

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeatedString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    for (let repetition = 0; repetition < num; repetition++) {
+        repeatedString = repeatedString + str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
