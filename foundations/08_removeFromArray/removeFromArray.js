83b169c92ac29ebd47434ce0c09d780bf86a2597
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-04 05:03:31 +0100

    Completed 08-removeFromArray
    created a function that removes multiple arguments from a given array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let val of args) {
+        for (let index = 0; index < array.length; index++) {
+            if (val === array[index]) {
+                array.splice(index, 1)
+                index--; //to ensure no element is skipped
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
