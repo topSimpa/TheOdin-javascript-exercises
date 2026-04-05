7f5e0a8074734d47b96de92c17f171541e8710cb
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-05 09:57:17 +0100

    Completed 09-SUMALL
    created a function that sums up a range of positive integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if ((start < 0) || (end < 0)) {
+        return "ERROR"; 
+    }
+       
+
+    if ((typeof start  != "number") || typeof end  != "number") {
+         return "ERROR";
+    }
+
+    if (!(Number.isInteger(start) && Number.isInteger(start))) {
+         return "ERROR";
+    }   
+
+    if (start > end) {
+         let buffer = start
+         start = end;
+         end = buffer;
+    }
+       
+
+    // console.log(`start ${start}`);
+    // console.log(`end ${end}`);
+
+    for (let number = start; number <= end; number++) {
+        sum += number;
+    }
+    return sum;
 };
 
 // Do not edit below this line
