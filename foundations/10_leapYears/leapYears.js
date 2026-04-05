0b0d46bb36688cf275e8652016b23607cc2833a5
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-05 12:06:43 +0100

    Completed 10-leapYears
    created a function that checks if a year is a leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year < 100)
+            return true;
+        else if (year % 100 != 0)
+            return true;
+        else {
+            if (year % 400 == 0)
+                return true;
+            else
+                return false;
+        }
+    } else {
+        return false;
+    }
 
 };
 
