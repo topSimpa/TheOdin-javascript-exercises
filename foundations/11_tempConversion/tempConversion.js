9fd9e534367da1a76f6201eb90411dcdcd86222b
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-05 12:33:08 +0100

    Completed 11-tempConversion
    created function that covert from fareheit to celcius and vice-versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let degreeCelcius = (temp - 32) * 5 / 9;
+
+  return (Math.round(degreeCelcius * 10) / 10) // to approximate to one decimal place
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let degreeF =  (9 * temp) / 5 + 32;
+
+  return (Math.round(degreeF * 10) / 10)
 };
 
 // Do not edit below this line
