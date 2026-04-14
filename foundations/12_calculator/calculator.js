20ad3bc27a764c1e72ccdf627f68dc9b8eb09873
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-14 16:32:30 +0100

    completed the calculator project

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(augEnd, addEnd) {
+  return (augEnd + addEnd);
 	
 };
 
-const subtract = function() {
+const subtract = function(minuEnd, subtraEnd) {
+  return (minuEnd - subtraEnd);
+};
+
+const sum = function(arr) {
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
 	
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce(
+    (product, currentValue) => product * currentValue,
+    1,
+  )
 };
 
-const multiply = function() {
-
+const power = function(base, exponent) {
+	return (base ** exponent);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(argument) {
+	let range = Array.from({ length: argument }, (_, i) => i + 1)
+  return (multiply(range))
 };
 
 // Do not edit below this line
