14764c3038def6e727392e000c0a68e65ac5009d
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-14 22:48:35 +0100

    completed palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //Remove spaces and punctuations
+    const strArray = str.split(/[^\w]+/);
+    console.log(strArray)
 
+    const filteredString = strArray.join("");
+    console.log(filteredString)
+
+    
+    let length = filteredString.length;
+    console.log(length);
+
+    //loop through the string halfway
+    const middle = Math.floor(length / 2);
+
+    for (let index = 0; index < middle; index++) {
+        const frontChar = filteredString[index].toLowerCase();
+        const rearChar = filteredString[--length].toLowerCase();
+
+        //check if each letter on both halves are same
+        if (frontChar != rearChar) {
+            return false
+        }
+
+    }
+    return true;
+    
 };
 
 // Do not edit below this line
