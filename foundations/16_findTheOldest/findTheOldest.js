a8a2cb9ed17b32a50b051f8052519c6f2b153919
topSimpa <mohammedabdulmujeeb16@gmail.com>
2026-04-15 14:45:32 +0100

    Completed findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(deceased) {
+    const currentYear  = new Date().getFullYear();
 
+    return deceased.reduce(
+        (oldest, currentPerson) => {
+            const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+            const currentPersonAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+            if (oldestAge > currentPersonAge) {
+                return oldest;
+            } 
+            return currentPerson;
+        }
+    );
 };
 
 // Do not edit below this line
